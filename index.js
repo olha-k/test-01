8155f169d219d76eae4af39990fc989001a7e183
olha-k <39318385+olha-k@users.noreply.github.com>
2019-07-29 16:15:05 -0400

    Sample code

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+let  a = 0;
+    b = 0;
+
+    const foo = function (a = 2) {
+        b= 3;
+    return function (b=4) {
+        return a + b;
+    }
+}
+
+console.log (foo()(5));
+
+let Neo;
+ setTimeout (() => {
+     let i = 0;
+
+     while ("it looks like you're waiting for something')
+ })
